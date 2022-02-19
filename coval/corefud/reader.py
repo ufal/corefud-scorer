4015e58c9389094403f8df34a3aee477c319500a
Michal Novak <mnovak@ufal.mff.cuni.cz>
2022-02-19 13:32:05 +0100

    works on corefud scorer started

---
+++ coval/corefud/reader.py
@@ -0,0 +1,20 @@
+from udapi.core.document import Document
+from udapi.block.read.conllu import Conllu
+
+def load_conllu(file_path):
+    doc = Document()
+    conllu_reader = Conllu(files=[file_path])
+    conllu_reader.apply_on_document(doc)
+    return doc
+
+
+def get_coref_infos(key_file,
+        sys_file,
+        keep_singletons,
+        print_debug=False):
+
+    key_doc = load_conllu(key_file)
+    sys_doc = load_conllu(sys_file)
+
+    key_doc.coref_clusters
+    sys_doc.coref_clusters
